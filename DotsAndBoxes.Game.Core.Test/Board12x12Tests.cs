a24b4262df133b6c1de9bd0bf1d19047a6017a63
paddyv1 <patricktaltykerr@outlook.com>
2026-02-27 19:05:24 +0000

    Switch test project to NUnit, rename, and clean up
    Renamed test project to DotsAndBoxes.Game.Core.Test and updated references in .gitignore and solution. Migrated from xUnit to NUnit, including Allure integration via Allure.NUnit. Rewrote Board12x12Tests for NUnit, removed EnumDtoTests, and replaced the old test project file with a new one targeting .NET 10.0. Cleaned up obsolete dependencies.

---
+++ DotsAndBoxes.Game.Core.Test/Board12x12Tests.cs
@@ -0,0 +1,28 @@
+﻿using Allure;
+using Allure.NUnit;
+
+namespace DotsAndBoxes.Game.Core.Test
+{
+    [AllureNUnit]
+    public class Board12x12Tests
+    {
+       
+
+        [Test]
+        public void CheckHeightAndWidthValuesAreExpected()
+        {
+            Assert.That(Board12x12.H, Is.EqualTo(12));
+            Assert.That(Board12x12.W, Is.EqualTo(12));
+
+        }
+
+        [Test]
+        public void Check_HEdge_VEdge_EdgeCount_BoxCount_ValuesAreExpected()
+        {
+            Assert.That(Board12x12.HEdgeCount, Is.EqualTo(156));
+            Assert.That(Board12x12.VEdgeCount, Is.EqualTo(156));
+            Assert.That(Board12x12.BoxCount, Is.EqualTo(144));
+            Assert.That(Board12x12.EdgeCount, Is.EqualTo(312));
+        }
+    }
+}
