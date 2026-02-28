ac958604a8663cf1116a9557acef20938a93bbee
paddyv1 <patricktaltykerr@outlook.com>
2026-02-28 22:17:16 +0000

    Implement GameEngine core logic and add unit tests
    Added GameEngine class with edge ownership tracking, player state, and scoring for Dots and Boxes. Implemented methods for getting, setting, and checking edge ownership. Introduced unit tests for these methods using NUnit and Allure.

---
+++ DotsAndBoxes.Game.Core.Test/GameEngineTests.cs
@@ -0,0 +1,64 @@
+﻿using Allure.NUnit;
+using DotsAndBoxes.Game.Core;
+using NUnit.Framework;
+using System;
+
+namespace DotsAndBoxes.Game.Core.Test
+{
+    [TestFixture]
+    [AllureNUnit]
+    public class GameEngineTests
+    {
+        [Test]
+        public void GetEdgeOwner_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var gameEngine = new GameEngine();
+            int edgeId = 0;
+
+            // Act
+            var result = gameEngine.GetEdgeOwner(
+                edgeId);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(-1));
+        }
+
+        [Test]
+        public void SetEdgeOwner_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var gameEngine = new GameEngine();
+            int edgeId = 0;
+            sbyte player = 0;
+
+            // Act
+            gameEngine.SetEdgeOwner(
+                edgeId,
+                player);
+
+            var result = gameEngine.GetEdgeOwner(edgeId);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(player));
+        }
+
+        [Test]
+        public void IsEdgeFree_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var gameEngine = new GameEngine();
+            int edgeId = 0;
+
+            // Act
+            var result = gameEngine.IsEdgeFree(
+                edgeId);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(true));
+        }
+
+
+        
+    }
+}
--- DotsAndBoxes.Game.Core/GameEngine.cs
+++ DotsAndBoxes.Game.Core/GameEngine.cs
@@ -6,5 +6,86 @@ namespace DotsAndBoxes.Game.Core
 {
     public class GameEngine
     {
+        public sbyte[] _hEdgeOwners = CreateArrayWithValue(156, -1);
+        public sbyte[] _vEdgeOwners = CreateArrayWithValue(156, -1);
+
+
+        public int currentPlayer { get; private set; } = 0;
+        public bool gameOver { get; private set; } = false;
+        public long stateVersion { get; private set; } = 0;
+        public int[] scores { get; } = new int[2];
+
+
+        public GameEngine()
+        {
+            
+        }
+
+        private static sbyte[] CreateArrayWithValue(int length, sbyte value)
+        {
+            var arr = new sbyte[length];
+            for (var i = 0; i < length; i++)
+            {
+                arr[i] = value;
+            }
+            return arr;
+        }
+
+        public sbyte GetEdgeOwner(int edgeId)
+        {
+            if(edgeId < 156)
+            {
+                return _hEdgeOwners[edgeId];
+            }
+            else
+            {
+                edgeId = edgeId - 156;
+                return _vEdgeOwners[edgeId];
+            }
+        }
+
+        public void SetEdgeOwner(int edgeId, sbyte player)
+        {
+            sbyte playerName1= 0, player2 = 1;
+
+
+            if(player != playerName1 || player2 != 1)
+            {
+                throw new ArgumentException();
+            }
+
+            if(edgeId > 155)
+            {
+                edgeId = edgeId - 156;
+                _vEdgeOwners[edgeId] = player;
+            }
+            else
+            {
+                _hEdgeOwners[edgeId] = player;
+            }
+
+        }
+
+        public bool IsEdgeFree(int edgeId)
+        {
+            if (edgeId < 156)
+            {
+                if (_hEdgeOwners[edgeId] == -1)
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                edgeId = edgeId - 156;
+                if (_vEdgeOwners[edgeId] == -1)
+                {
+                    return true;
+                }
+            }
+
+
+            return false;
+        }
     }
 }
